ddf512756846b3afffcd097e8d79b0b4c54f548e
DoğukanKorkmaz <dkorkmaz2000@hotmail.com>
2023-01-16 13:23:31 +0300

    Oyuna bölümler eklendi.
    2.ve3.bölüm eklendi.

--- Havoc/Assets/Scripts/tugla.cs
+++ Havoc/Assets/Scripts/tugla.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class tugla : MonoBehaviour
 {
+    public static int toplamTuglaSayisi;
     public Sprite[] tuglaSprite;
     private int maxCarpmaSayisi;
     private int carpmaSayisi;
     void Start()
     {
-        maxCarpmaSayisi = tuglaSprite.Length + 1 ;
+        maxCarpmaSayisi = tuglaSprite.Length + 1;
+        toplamTuglaSayisi++;
     }
 
     // Update is called once per frame
@@ -25,6 +27,12 @@ public class tugla : MonoBehaviour
             carpmaSayisi++;
             if (carpmaSayisi >= maxCarpmaSayisi)
             {
+                toplamTuglaSayisi--;
+                Debug.Log(toplamTuglaSayisi);
+                if (toplamTuglaSayisi <=0)
+                {
+                    GameObject.FindObjectOfType<OyunKontrol>().GetComponent<OyunKontrol>().BirSonrakiSahne();
+                }
                 Destroy(this.gameObject);
             }
             else

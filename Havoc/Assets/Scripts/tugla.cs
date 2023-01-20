bb890a7febe624dc8a5ed91cd3aa864e17b35dbe
DoğukanKorkmaz <dkorkmaz2000@hotmail.com>
2023-01-20 18:51:59 +0300

    Efektler eklendi.
    Çarpma ve kırılma efektleri eklendi

--- Havoc/Assets/Scripts/tugla.cs
+++ Havoc/Assets/Scripts/tugla.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class tugla : MonoBehaviour
 {
+    public GameObject kirilmaEfekti;
     public AudioClip sesTuglaKirilma;
     public AudioClip sesTuglaCarpma;
     public static int toplamTuglaSayisi;
@@ -38,6 +39,11 @@ public class tugla : MonoBehaviour
                 {
                     GameObject.FindObjectOfType<OyunKontrol>().GetComponent<OyunKontrol>().BirSonrakiSahne();
                 }
+                Vector3 pos = diger.contacts[0].point;
+                GameObject go = Instantiate(kirilmaEfekti, pos, Quaternion.identity) as GameObject;
+                Color tuglaRengi = GetComponent<SpriteRenderer>().color;
+                go.GetComponent<ParticleSystemRenderer>().material.color = tuglaRengi;
+                Destroy(go, 1f);
                 AudioSource.PlayClipAtPoint(sesTuglaKirilma, transform.position);
                 Destroy(this.gameObject);
             }
